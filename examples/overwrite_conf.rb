b4023230446911e4f1afb5968928b7436af48923
Natenom <natenom@gmail.com>
2015-08-15 23:47:39 +0200

    add some more variables into the overwrite config in examples

--- examples/overwrite_conf.rb
+++ examples/overwrite_conf.rb
@@ -5,8 +5,11 @@ def ext_config()
                     mumbleserver_port: 64738,
                     mumbleserver_username: "Name_of_your_bot",
                     mumbleserver_targetchannel: "Bottest",
+                    quality_bitrate: 72000,
+                    stop_on_unregistered_users: true,
                     mpd_fifopath: "/home/botmaster/mpd1/mpd.fifo",
                     mpd_port: 7701,
                  }
+
     @settings = @settings.merge(overwrite)
 end
