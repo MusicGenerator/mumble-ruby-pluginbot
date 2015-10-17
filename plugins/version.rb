5bdfaade8f9e8278f1131816208bd59a9794923b
dafoxia <reinhard.bramel@liwest.at>
2015-10-17 17:38:11 +0200

    #34 done by git command. (will not work if bot not cloned with git)

---
+++ plugins/version.rb
@@ -0,0 +1,28 @@
+class Version < Plugin
+
+    def init(init)
+        super
+        @@bot[:bot] = self
+        return @@bot
+        #nothing to init
+    end
+    
+    def name
+        self.class.name
+    end
+    
+    def help(h)
+        h += "<hr><span style='color:red;'>Plugin #{self.class.name}</span><br />"
+        h += "<b>#{@@bot[:controlstring]}version</b> - Show the used Bot version.<br />"
+        h
+    end
+   
+    def handle_chat(msg, message)
+        super
+        if message == "version"
+            versionshort = `git rev-parse --short HEAD`
+            versionlong = `git rev-parse HEAD` 
+            privatemessage("Version: " + versionshort.to_s + " / " +versionlong.to_s)
+        end
+    end
+end
