da3b7027d5f86c7b94d4fe226ea2b9b481ca7315
Natenom <natenom@natenom.com>
2015-11-03 00:18:54 +0100

    text fixes

--- pluginbot.rb
+++ pluginbot.rb
@@ -389,7 +389,7 @@ class MumbleMPD
                                 help += "<b>#{cc}reset</b> Reset variables to default value. Needs binding!<br />"
                                 help += "<b>#{cc}restart</b> Restart Bot. Needs binding.<br />"
                                 help += "<b>#{cc}blacklist <i>username</i></b> Add user to blacklist. Needs binding.<br />"
-                                help += "<b>#{cc}register</b>Register the bot on the current server. Works only if server allows it. If it doesn't work ask an administrator of your Mumble server.<br />"
+                                help += "<b>#{cc}register</b> Let the bot register itself on the current server. Works only if server allows it. If it doesn't work ask an administrator of your Mumble server. Be aware that after registration only an administrator can change the name of the bot.<br />"
                                 help += "<b>#{cc}ducking</b> toggle voice ducking on/off.<br />"
                                 help += "<b>#{cc}duckvol <i>volume</i></b> set the ducking volume (% of normal volume).<br />"
 
