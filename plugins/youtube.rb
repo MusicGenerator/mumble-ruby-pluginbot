617efb33a020cacf140b062c31cdea86ad850174
Natenom <natenom@natenom.com>
2015-12-21 02:53:43 +0100

    language

--- plugins/youtube.rb
+++ plugins/youtube.rb
@@ -12,7 +12,7 @@ class Youtube < Plugin
         Dir.mkdir(@youtubefolder) unless File.exists?(@youtubefolder)
         Dir.mkdir(@tempyoutubefolder) unless File.exists?(@tempyoutubefolder)
       rescue
-        puts "Error: Youtube-Plugin doesn't found settings for mpd music directory and/or your preferred temporary download directory"
+        puts "Error: Youtube-Plugin didn't find settings for mpd music directory and/or your preferred temporary download directory"
         puts "See pluginbot_conf.rb"
       end
       begin
