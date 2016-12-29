48079c3dbc61bee8b7b82dbc8986d7b36b7ec5dc
Natenom <natenom@natenom.com>
2016-12-29 09:10:44 +0100

    implement .branch and .commiturl

--- plugins/version.rb
+++ plugins/version.rb
@@ -15,6 +15,8 @@ class Version < Plugin
   def help(h)
     h << "<hr><span style='color:red;'>Plugin #{self.class.name}</span><br>"
     h << "<b>#{@@bot["main"]["control"]["string"]}version</b> - Show the used Bot version.<br>"
+    h << "<b>#{@@bot["main"]["control"]["string"]}branch</b> - Show the used branch name.<br>"
+    h << "<b>#{@@bot["main"]["control"]["string"]}commiturl</b> - Print a clickable commit URL based on the assumption that it is a commit in the main repository.<br>"
     h
   end
 
@@ -24,8 +26,18 @@ class Version < Plugin
       versionshort = `git rev-parse --short HEAD`
       versionlong = `git rev-parse HEAD`
       date = `git log -n1 --format="%at"`
+
+      privatemessage("Version: #{versionshort.to_s} / #{versionlong.to_s} / #{Time.at(date.to_i).utc}")
+    end
+
+    if message == "branch"
       branch = `git rev-parse --abbrev-ref HEAD`
-      privatemessage("Version: #{versionshort.to_s} / #{versionlong.to_s} / #{Time.at(date.to_i).utc} / Branch: #{branch.to_s} / <a href='https://github.com/dafoxia/mumble-ruby-pluginbot/commit/#{versionlong.to_s}'>(URL)</a>")
+      privatemessage("Branch: #{branch.to_s}")
+    end
+
+    if message == "commiturl"
+      versionlong = `git rev-parse HEAD`
+      privatemessage("<a href='https://github.com/dafoxia/mumble-ruby-pluginbot/commit/#{versionlong.to_s}'>#{versionlong.to_s}</a>")
     end
   end
 end
