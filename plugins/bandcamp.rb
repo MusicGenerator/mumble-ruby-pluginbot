0fca0b4c4e070a5fb6ec2d3c6fc19a752e16442e
dafoxia <dafoxiagit@gmail.com>
2016-12-29 21:57:15 +0100

    I18n Support for Plugin Bandcamp

--- plugins/bandcamp.rb
+++ plugins/bandcamp.rb
@@ -39,15 +39,15 @@ class Bandcamp < Plugin
 
   def help(h)
       h << "<hr><span style='color:red;'>Plugin #{self.class.name}</span><br>"
-      h << "<b>#{@@bot["main"]["control"]["string"]}bandcamp <i>URL</i></b> - Will try to download the music from the given URL. Be aware that due to bandwidth limitations from bandcamp the bot downloads with the a maximum speed that is slightly higher than the streaming speed. Please be patient if you let the bot download a whole album :)<br>"
-      h << "<b>#{@@bot["main"]["control"]["string"]}ytdl-version</b> - print used download helper version"
+      h << "<b>#{@@bot["main"]["control"]["string"]}bandcamp <i>URL</i></b> - #{I18n.t("plugin_bandcamp.help.bandcamp")}<br>"
+      h << "<b>#{@@bot["main"]["control"]["string"]}ytdl-version</b> - #{I18n.t("plugin_bandcamp.help.ytdl_version")}"
   end
 
   def handle_chat(msg, message)
     super
 
     if message == "ytdl-version"
-      privatemessage("Bandcamp uses youtube-dl " + `#{@@bot["plugin"]["bandcamp"]["youtube_dl"]["path"]} --version`)
+      privatemessage("#{I18n.t("plugin_bandcamp.ytdlversion", :version => `#{@@bot["plugin"]["bandcamp"]["youtube_dl"]["path"]} --version`)}")
     end
 
     if message.start_with?("bandcamp <a href=") || message.start_with?("<a href=") then
@@ -65,24 +65,24 @@ class Bandcamp < Plugin
           Thread.current["user"]=actor
           Thread.current["process"]="bandcamp"
 
-          messageto(actor, "Bandcamp is inspecting link: " + link + "...")
+          messageto(actor, I18n.t("plugin_bandcamp.inspecting", :link => link ))
           get_song link
           if ( @songlist.size > 0 ) then
             @@bot[:mpd].update(@@bot["plugin"]["bandcamp"]["folder"]["download"].gsub(/\//,""))
-            messageto(actor, "Waiting for database update complete...")
+            messageto(actor, I18n.t("plugin_bandcamp.db_update"))
 
             while @@bot[:mpd].status[:updating_db] do
               sleep 0.5
             end
 
-            messageto(actor, "Update done.")
+            messageto(actor, I18n.t("plugin_bandcamp.db_update_done"))
             while @songlist.size > 0
               song = @songlist.pop
               messageto(actor, song)
               @@bot[:mpd].add(@@bot["plugin"]["bandcamp"]["folder"]["download"]+song)
             end
           else
-            messageto(actor, "Bandcamp: The link contains nothing interesting.")
+            messageto(actor, I18n.t("plugin_bandcamp.badlink"))
           end
         end
       end
